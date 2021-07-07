a606e1051fe80a9e462b2cc3ba75a86f58c113cc
NikhilMeh <nikhil221205@gmail.com>
2021-07-07 19:31:19 +0530

    working with length function in java.

---
+++ src/com/company/Length_String.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Length_String {
+    public static void main(String args[]){
+        String s1="BoardExams";
+        String s2="Covid";
+        System.out.println("The length of s1 is :"+s1.length());
+        System.out.println("The length of s2 is :"+s2.length());
+    }
+}
