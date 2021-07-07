41989e6cca62057b683572b8158dc2d570ce7c57
NikhilMeh <nikhil221205@gmail.com>
2021-07-07 19:55:46 +0530

    working with Replace function in java.

---
+++ src/Replace_String.java
@@ -0,0 +1,6 @@
+public class Replace_String {
+    public static void main(String args[]){
+        String s1="India Lose in WTC Final";
+        System.out.println(s1.replace("WTC","WorldCup"));
+    }
+}
